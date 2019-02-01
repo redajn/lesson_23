20d6f6233aba4dceb11bfc0932965d957340db3b
redajn <redajn@yandex.ru>
2019-02-01 10:04:12 +0300

    js move to head

--- app.rb
+++ app.rb
@@ -13,3 +13,14 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	@username = params[:username]
+	@datetime = params[:datetime]
+	@phone = params[:phone]
+	@barber = params[:barber]
+
+
+	erb "Done! We are waiting you at #{@datetime} and call to you wiyh number #{@phone}, #{@username}. Your barber is #{@barber}"
+end
+
